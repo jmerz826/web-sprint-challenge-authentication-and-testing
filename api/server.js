09fdd204e771c6101069c1283c9e928e7af46eeb
John Merz <jmerz826@gmail.com>
2022-01-28 13:16:52 -0500

    built error catcher in server

--- api/server.js
+++ api/server.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
-const restrict = require('./middleware/restricted.js');
+const restrict = require('./middleware/restricted.js')
 
-const authRouter = require('./auth/auth-router.js');
-const jokesRouter = require('./jokes/jokes-router.js');
+const authRouter = require('./auth/auth-router.js')
+const jokesRouter = require('./jokes/jokes-router.js')
 
-const server = express();
+const server = express()
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+server.use('/api/auth', authRouter)
+server.use('/api/jokes', restrict, jokesRouter) // only logged-in users should have access!
 
-module.exports = server;
+// error catcher
+server.use('/', (err, req, res, next) => { // eslint-disable-line 
+    res.status(err.status || 500).json(err.message)
+})
+
+module.exports = server
